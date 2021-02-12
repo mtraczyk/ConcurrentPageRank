63e8400f87c0759b7b4d76ae1be41cf761c09074
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-02-12 23:15:37 +0100

    Sha256IdGenerator implemented.

--- src/sha256IdGenerator.hpp
+++ src/sha256IdGenerator.hpp
@@ -3,12 +3,36 @@
 
 #include "immutable/idGenerator.hpp"
 #include "immutable/pageId.hpp"
+#include <iostream>
+#include <fstream>
 
 class Sha256IdGenerator : public IdGenerator {
-public:
-    virtual PageId generateId(std::string const& /*content*/) const
-    {
-        return PageId("");
+  public:
+    virtual PageId generateId(std::string const &content) const {
+      char buffer[64];
+      std::string hashValue;
+      std::ofstream contentFile("content.txt");
+
+      contentFile << content;
+
+      FILE *pipe = popen("sha256sum content.txt", "r");
+
+      if (pipe == nullptr) {
+        throw std::runtime_error("popen() failed!");
+      }
+
+      if (fgets(buffer, 64, pipe) != nullptr) {
+        hashValue = buffer;
+      }
+
+      pclose(pipe);
+      contentFile.close();
+
+      if (remove("content.txt") != 0) {
+        throw std::runtime_error("Removal of a file failed!");
+      }
+
+      return PageId(hashValue);
     }
 };
 
