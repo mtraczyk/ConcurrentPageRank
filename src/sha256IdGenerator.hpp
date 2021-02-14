a5c1cf246c67e2175a27b2b499e2652cd64d1e45
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-02-14 18:32:42 +0100

    Some changes

--- src/sha256IdGenerator.hpp
+++ src/sha256IdGenerator.hpp
@@ -12,18 +12,7 @@ class Sha256IdGenerator : public IdGenerator {
     virtual PageId generateId(std::string const &content) const {
       char buffer[64];
       std::string hashValue;
-      static std::atomic<int> counter(0);
-      std::string fileName = "content" + std::to_string(counter.fetch_add(1)) + ".txt";
-      std::fstream contentFile(fileName, std::ios::in | std::ios::out | std::ios::trunc);
-
-      if (!contentFile) {
-        throw std::runtime_error("Could not open a file.");
-      }
-
-      contentFile << content;
-      contentFile.close();
-
-      std::string aux = "sha256sum " + fileName;
+      std::string aux = "echo " + content + " | sha256sum";
       FILE *pipe = popen(aux.c_str(), "r");
 
       if (pipe == nullptr) {
@@ -33,14 +22,10 @@ class Sha256IdGenerator : public IdGenerator {
       if (fscanf(pipe, "%64s", buffer) == 1) {
         hashValue = buffer;
       } else {
-        throw std::runtime_error("fgets() failed!");
+        throw std::runtime_error("fscanf() failed!");
       }
 
       pclose(pipe);
-
-      if (remove(fileName.c_str()) != 0) {
-        throw std::runtime_error("Removal of a file failed!");
-      }
 
       return PageId(hashValue);
     }
