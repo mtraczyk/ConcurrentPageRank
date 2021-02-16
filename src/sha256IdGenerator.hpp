857b72d3a0d2abe1d9a3dcf45ca1ea5025b44862
Michał Traczyk <mt418401@students.mimuw.edu.pl>
2021-02-16 11:19:47 +0100

    Reversion

--- src/sha256IdGenerator.hpp
+++ src/sha256IdGenerator.hpp
@@ -12,7 +12,18 @@ class Sha256IdGenerator : public IdGenerator {
     virtual PageId generateId(std::string const &content) const {
       char buffer[64];
       std::string hashValue;
-      std::string aux = "echo -n '" + content + "' | sha256sum";
+      static std::atomic<int> counter(0);
+      std::string fileName = "content" + std::to_string(counter.fetch_add(1)) + ".txt";
+      std::fstream contentFile(fileName, std::ios::in | std::ios::out | std::ios::trunc);
+
+      if (!contentFile) {
+        throw std::runtime_error("Could not open a file.");
+      }
+
+      contentFile << content;
+      contentFile.close();
+
+      std::string aux = "sha256sum " + fileName;
       FILE *pipe = popen(aux.c_str(), "r");
 
       if (pipe == nullptr) {
@@ -22,10 +33,14 @@ class Sha256IdGenerator : public IdGenerator {
       if (fscanf(pipe, "%64s", buffer) == 1) {
         hashValue = buffer;
       } else {
-        throw std::runtime_error("fscanf() failed!");
+        throw std::runtime_error("fgets() failed!");
       }
 
       pclose(pipe);
+
+      if (remove(fileName.c_str()) != 0) {
+        throw std::runtime_error("Removal of a file failed!");
+      }
 
       return PageId(hashValue);
     }
